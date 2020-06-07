8ac6f4cb347b29a15f359a8ce79fb268fa94b675
cfbonner <cfbonner@gmail.com>
2020-06-07 22:39:35 +0100

    Add message persistence

--- lib/tattler_web/channels/room_channel.ex
+++ lib/tattler_web/channels/room_channel.ex
@@ -9,12 +9,13 @@ defmodule TattlerWeb.RoomChannel do
     case Tattler.Repo.get_by(Chat.Room, friendly_id: private_room_id) do
       %Chat.Room{} -> {:ok, socket}
       nil ->
-        Chat.create_room(%{friendly_id: private_room_id})
+        spawn(fn -> Chat.create_room(%{friendly_id: private_room_id}) end)
         {:ok, socket}
     end
   end
 
   def handle_in("new_message", %{"name" => name, "body" => body}, socket) do
+    spawn(fn -> Chat.create_message(%{name: name, body: body}) end)
     broadcast!(socket, "new_message", %{name: name, body: body})
     {:noreply, socket}
   end
