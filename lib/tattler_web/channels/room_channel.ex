139dbc152c6fd63d13b2215746c0b5aa66d384bf
cfbonner <cfbonner@gmail.com>
2020-06-07 22:33:04 +0100

    create a new room when joined

--- lib/tattler_web/channels/room_channel.ex
+++ lib/tattler_web/channels/room_channel.ex
@@ -1,12 +1,17 @@
 defmodule TattlerWeb.RoomChannel do
   use Phoenix.Channel
+  alias Tattler.Chat
   
   def join("room:lobby", _message, socket) do
     {:ok, socket}
   end
-  def join("room:" <> _private_room_id, _params, socket) do
-    {:ok, socket}
-    # {:error, %{reason: "unauthorized"}}
+  def join("room:" <> private_room_id, _params, socket) do
+    case Tattler.Repo.get_by(Chat.Room, friendly_id: private_room_id) do
+      %Chat.Room{} -> {:ok, socket}
+      nil ->
+        Chat.create_room(%{friendly_id: private_room_id})
+        {:ok, socket}
+    end
   end
 
   def handle_in("new_message", %{"name" => name, "body" => body}, socket) do
